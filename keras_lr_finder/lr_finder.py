feb9970091d51339a311a8e2ebdfc38fdd5b37ec
Jonathan Mackenzie <jonmac1@gmail.com>
2019-07-15 13:12:55 +0930

    Update lr_finder.py
    fixed generator bug

--- keras_lr_finder/lr_finder.py
+++ keras_lr_finder/lr_finder.py
@@ -40,7 +40,7 @@ class LRFinder:
 
     def find(self, x_train, y_train, start_lr, end_lr, batch_size=64, epochs=1):
         num_batches = epochs * x_train.shape[0] / batch_size
-        self.lr_mult = (float(end_lr) / float(start_lr)) ** (float(1) / float(num_batches))
+        self.lr_mult = (float(end_lr) / float(start_lr)) ** (1.0 / float(num_batches)*float(epochs))
 
         # Save weights into a file
         self.model.save_weights('tmp.h5')
